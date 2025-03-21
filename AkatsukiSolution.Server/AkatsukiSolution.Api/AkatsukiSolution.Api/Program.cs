d0dd55dbfb552725a74935217a18c0a95dc6f6f1
cyberjian19 <cyberjian96@gmail.com>
2025-03-21 14:20:59 +0100

    initial commit

---
+++ AkatsukiSolution.Server/AkatsukiSolution.Api/AkatsukiSolution.Api/Program.cs
@@ -0,0 +1,27 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    builder.AllowAnyOrigin()
+           .AllowAnyHeader()
+           .AllowAnyMethod());
+});
+
+var app = builder.Build();
+app.UseCors("AllowAll");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/ping", () =>
+{
+    return Results.Ok("200 OK, ajmо da pokidamo!");
+})
+.WithName("Ping")
+.WithOpenApi();
+
+app.Run();
