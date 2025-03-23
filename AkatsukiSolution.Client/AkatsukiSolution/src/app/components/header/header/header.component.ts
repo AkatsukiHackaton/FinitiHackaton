95efdb35cef0f2ce6df9f4413892113361419b68
cyberjian19 <cyberjian96@gmail.com>
2025-03-23 01:36:39 +0100

    styling changes

--- AkatsukiSolution.Client/AkatsukiSolution/src/app/components/header/header/header.component.ts
+++ AkatsukiSolution.Client/AkatsukiSolution/src/app/components/header/header/header.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 
+export class panelItem  {
+  id!: number;
+  panel!: string;
+  active!: boolean;
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
 
-  items = [
-    'FINITI izveštaji',
-    'Administracija',
-    "Zaposleni"
-  ]
-
+  items:panelItem[] = [
+    { id: 1,panel: 'FINITI izveštaji', active: false },
+    {id:2,panel: 'Administracija', active: false },
+    {id:3, panel: 'Zaposleni', active: false },
+  ];
+  toggle(index: number) {
+    this.items.forEach((item: panelItem, i:number) => i === index ? item.active = true : item.active = false);
+    this.items[index].active = true;
+  }
 }
